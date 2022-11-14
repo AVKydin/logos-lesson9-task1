154c7dc2ec43610091f50212f1fe3e0e859917f0
AVKydin <tlalok@ukr.net>
2022-11-14 11:44:39 +0200

    end task1 L9

--- main.js
+++ main.js
@@ -3,57 +3,29 @@ let down = document.querySelector(".down");
 let slide1 = document.querySelector(".slide1");
 let slide2 = document.querySelector(".slide2");
 let up = document.querySelector(".scroll-up");
+let up2 = document.querySelector(".up");
+
 
 window.addEventListener('scroll', function (e) {
     if (window.scrollY >= 50 && window.scrollY <= 400) {
-      down.style.fontSize = `${window.scrollY / 5}px`;
+      down.style.fontSize = `${26 + window.scrollY / 10}px`;
     }
 
     if (window.scrollY >= 400 && window.scrollY <= 1000) {
-        slide1.style.width = `${window.scrollY / 2}px`;
-        slide1.style.marginLeft = `${window.scrollY / 3}px`;
-
-        
+        slide1.style.width = `${200 + (window.scrollY - 400) / 3}px`;
+        slide1.style.marginLeft = `${200 + (window.scrollY - 400) / 4}px`;
+        slide2.style.marginRight = `${230 + (window.scrollY - 400) / 3}px`;
     }
-
-    if (window.scrollY >= 800 && window.scrollY <= 1400) {
-        slide2.style.marginRight = `${window.scrollY / 2}px`;
+  
+    if (window.scrollY >= 800 && window.scrollY <= 1500) {
+      up.style.fontSize = `${72 - (window.scrollY - 800) / 10}px`;
     }
-      if (window.scrollY >= 800 && window.scrollY <= 1500) {
-        up.style.fontSize = `${window.scrollY / 30}px`;
-      }
 })
 
-// "use strict";
+down.addEventListener('click', () => {
+  slide1.scrollIntoView({ behavior: 'smooth' });
+})
 
-// let scrollDown = document.querySelector(".scrollDown");
-// let down = document.querySelector(".down");
-// let slide = document.querySelector(".slide");
-// let image = document.querySelector(".image");
-// let up = document.querySelector(".up");
-
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY < 400) {
-//     down.style.fontSize = `${40 + window.scrollY / 12}px`;
-//   }
-//   if (window.scrollY > 300 && window.scrollY < 740) {
-//     slide.style.width = `${290 + (window.scrollY - 300) / 2}px`;
-//     slide.style.marginLeft = `${50 + (window.scrollY - 300) / 4}px`;
-//     image.style.right = `${10 + (window.scrollY - 300) / 3}px`;
-//   }
-//   if (window.scrollY > 800 && window.scrollY < 1200) {
-//     up.style.fontSize = `${74 - (window.scrollY - 800) / 12}px`;
-//   }
-// });
-
-// down.addEventListener("click", () => {
-//   slide.scrollIntoView({ behavior: "smooth" });
-// });
-
-// up.addEventListener("click", () => {
-//   window.scroll({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// });
-
+up2.addEventListener('click', () => {
+  window.scroll({top: 0, behavior: 'smooth'})
+})
